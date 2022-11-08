41afbefc8efe071561c1c46bc5265be21a800064
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2022-11-08 16:48:50 +0100

    first commit

---
+++ userHandlers.js
@@ -0,0 +1,72 @@
+const database = require("./database");
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+  
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the users");
+    });
+
+};
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users( firstname, lastname, email, city, language) VALUES ( ?, ?, ?, ?, ?)",
+      [ firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the users");
+    });
+};
+
+const getUsers = (req, res) => {
+  database
+  .query("select * from users")
+  .then(([users]) => {
+    res.json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data");
+  });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const user = users.find((user) => user.id === id);
+
+  if (user != null) {
+    res.json(user);
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  updateUser,
+  postUser, // don't forget to export your function ;)
+
+};
